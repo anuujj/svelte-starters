ba481338c5b169197615e3d2e5fbfae68256987d
anuujj <anujk8097@gmail.com>
2024-03-08 03:03:49 +0530

    reactivity, styling and js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
 <script>
-    let name = "Anuj";
+  import Para from './Para.svelte';
+  let name = "Anuj";
+  let htmlString = "this is a <em>emphasis</em> text";
+  let src = "/favicon.png";
 </script>
+
 <h1>Welcome {name}</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<img {src} alt="svelte fav-icon" />
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  {@html htmlString}
+</p>
+<Para/>
+
+<style>
+  p {
+    background-color: antiquewhite;
+    border: 1px solid rgb(48, 47, 47);
+    padding: 1em;
+  }
+</style>
---
+++ src/routes/Para.svelte
@@ -0,0 +1,15 @@
+<script>
+    let counter = 0;
+    function increment(){
+        counter+=1;
+    }
+    function decrement(){
+        counter-=1;
+    }
+</script>
+<p>para from another component</p>
+<div>
+    <button on:click={increment}>+</button>
+    <span>Count = {counter}</span>
+    <button on:click={decrement}>-</button>
+</div>
