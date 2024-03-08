fc3e682857f6db9ab9a4846189ad4eb9cd843509
anuujj <anujk8097@gmail.com>
2024-03-08 17:46:17 +0530

    conditionally rendering html

--- src/routes/Para.svelte
+++ src/routes/Para.svelte
@@ -1,17 +1,25 @@
 <script>
-    let counter = 0;
-    function increment(){
-        counter+=1;
-    }
-    function decrement(){
-        counter-=1;
-    }
-    export let description;
+  let counter = 0;
+  function increment() {
+    counter += 1;
+  }
+  function decrement() {
+    counter -= 1;
+  }
+  export let description;
 </script>
+
 <p>para from another component</p>
 <p>{description}</p>
 <div>
-    <button on:click={increment}>+</button>
-    <span>Count = {counter}</span>
-    <button on:click={decrement}>-</button>
+  <button on:click={increment}>+</button>
+  <span>Count = {counter}</span>
+  <button on:click={decrement}>-</button>
+  {#if counter > 10}
+    <p>Count is really high</p>
+  {:else if counter > 0}
+    <p>count is positive</p>
+  {:else}
+    <p>count is really low</p>
+  {/if}
 </div>
