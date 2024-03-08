560191e3a9174fb4c6251a1afdc4d8f6f49a9a83
anuujj <anujk8097@gmail.com>
2024-03-08 18:32:22 +0530

    rendering list with each

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,32 @@
 <script>
-  import Para from './Para.svelte';
+  import Para from "./Para.svelte";
   let name = "Anuj";
   let htmlString = "this is a <em>emphasis</em> text";
   let src = "/favicon.png";
   let description = "this is description from main page";
+  let groceryList = ["tomato", "onion", "potato", "eggs", "milk", "oil"];
 </script>
 
 <h1>Welcome {name}</h1>
 <img {src} alt="svelte fav-icon" />
 <p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
   {@html htmlString}
 </p>
-<Para {description}/>
+<ul>
+  {#each groceryList as item (item)}
+    <li>{item}</li>
+  {/each}
+</ul>
+<Para {description} />
 
 <style>
   p {
     background-color: antiquewhite;
     border: 1px solid rgb(48, 47, 47);
+    padding: 1em;
+  }
+  ul{
+    list-style: none;
     padding: 1em;
   }
 </style>
